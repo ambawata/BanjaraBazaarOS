c75571844eb8dbc078c3c1d1df2a68809c9fdada
Vinod Ambawata <laundryguy.in@gmail.com>
2026-07-01 11:43:43 +0530

    feat: implement Geometry Engine utilities for scale conversion, wall and room vectors, polygon math, and compass snapping

---
+++ apps/vastu-griha/src/lib/geometry/geometryUtils.js
@@ -0,0 +1,19 @@
+import { coordinateSystem } from './coordinateSystem'
+import { measurementEngine } from './measurementEngine'
+import { wallEngine } from './wallEngine'
+import { roomEngine } from './roomEngine'
+import { rotationEngine } from './rotationEngine'
+import { compassEngine } from './compassEngine'
+import { snapEngine } from './snapEngine'
+import { validationEngine } from './validationEngine'
+
+export {
+  coordinateSystem,
+  measurementEngine,
+  wallEngine,
+  roomEngine,
+  rotationEngine,
+  compassEngine,
+  snapEngine,
+  validationEngine
+}
